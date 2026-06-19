0bdb0cd2c2268e4fdd098f093f376fc630f6cd2e
vongockhang06 <vongockhang2006@gmail.com>
2026-06-19 14:56:16 +0800

    Delete create engine function because we will need to connect to db with many retries in app.py so we can use its engine => do not need to additionally create_engine in load

--- small_proj/load.py
+++ small_proj/load.py
@@ -1,16 +1,6 @@
 from sqlalchemy import text, create_engine
 from dotenv import load_dotenv
 import os
-load_dotenv
-def get_engine():
-    url=(
-        f"postgresql+psycopg2://"
-        f"postgres:{os.getenv('DB_PASSWORD', 'password')}"
-        f"@{os.getenv('DB_HOST', 'localhost')}"
-        f"/{os.getenv('DB_NAME', 'testdb')}"
-    )
-    return create_engine(url=url)
-
 def create_table(engine):
     with engine.begin() as conn:
         conn.execute(text("""
@@ -27,7 +17,7 @@ def load(engine, records:list):
     with engine.begin() as conn:
         for row in records:
             conn.execute(text("""
-                INSERT INTO crypto_prices
+                INSERT INTO crypto_prices (coin, price_usd, usd_24h_change, collected_at)
                 VALUES (:coin, :price_usd, :usd_24h_change, :collected_at)
             """),row)
     print('Load successfully')
